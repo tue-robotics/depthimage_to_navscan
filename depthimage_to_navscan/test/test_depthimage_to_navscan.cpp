84375520eb1eaaec645ec3026f60b66ae5bb6207
PetervDooren <PetervDooren@outlook.com>
2022-10-07 10:52:36 +0200

    apply fixture

--- depthimage_to_navscan/test/test_depthimage_to_navscan.cpp
+++ depthimage_to_navscan/test/test_depthimage_to_navscan.cpp
@@ -9,13 +9,7 @@ class DepthSensorIntegratorTest : public ::testing::Test {
 protected:
     void SetUp() override
     {
-        // configure camera model
-        cam_model.fx = 0.05;
-        cam_model.fy = 0.05;
-        cam_model.cx = 0.0;
-        cam_model.cy = 0.0;
-        cam_model.Tx = 0.0;
-        cam_model.Ty = 0.0;
+        //#TODO configure camera model
 
         dsi_cam_init.setCameraModel(cam_model);
 
@@ -25,22 +19,22 @@ protected:
         dsi_full_init.initialize(slope_threshold, min_distance, max_distance, num_samples, slope_window_size);
     }
 
-    DepthSensorIntegrator dsi_0;
+    DepthSensorIntegrator dsi_no_init;
     DepthSensorIntegrator dsi_params_init;
     DepthSensorIntegrator dsi_cam_init;
     DepthSensorIntegrator dsi_full_init;
 
     // sample cameramodel
-    image_geometry::PinholeCameraModel cam_model
+    image_geometry::PinholeCameraModel cam_model;
     // sample config
     uint num_samples = 20;
     double slope_threshold = 1.0;
     double min_distance = 0.4;
     double max_distance = 2.0;
     int slope_window_size = 30;
-}
+};
 
-TEST(NavscanTest, CheckNotInitialised)
+TEST_F(DepthSensorIntegratorTest, CheckNotInitialised)
 {
 std::vector<geo::Vector3> measurements;
 
@@ -48,13 +42,11 @@ std::vector<geo::Vector3> measurements;
 cv::Mat depth;
 geo::Pose3D sensor_pose;
 
-DepthSensorIntegrator depthSensorIntegrator;
-
-ASSERT_FALSE(depthSensorIntegrator.isInitialized()) << "depthSensorIntegrator believes itself to be initialized upon construction";
-ASSERT_FALSE(depthSensorIntegrator.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when not initialised";
+ASSERT_FALSE(dsi_no_init.isInitialized()) << "depthSensorIntegrator believes itself to be initialized upon construction";
+ASSERT_FALSE(dsi_no_init.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when not initialised";
 }
 
-TEST(NavscanTest, CheckCameraNotInitialised)
+TEST_F(DepthSensorIntegratorTest, CheckCameraNotInitialised)
 {
 std::vector<geo::Vector3> measurements;
 
@@ -62,19 +54,33 @@ std::vector<geo::Vector3> measurements;
 cv::Mat depth;
 geo::Pose3D sensor_pose;
 
-DepthSensorIntegrator depthSensorIntegrator;
-
-uint num_samples = 20;
-double slope_threshold = 1.0;
-double min_distance = 0.4;
-double max_distance = 2.0;
-int slope_window_size = 30;
-depthSensorIntegrator.initialize(slope_threshold, min_distance, max_distance, num_samples, slope_window_size);
-
-ASSERT_FALSE(depthSensorIntegrator.isInitialized()) << "depthSensorIntegrator believes itself to be initialized even when camera model not set";
-ASSERT_FALSE(depthSensorIntegrator.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when camera model not set";
+ASSERT_FALSE(dsi_params_init.isInitialized()) << "depthSensorIntegrator believes itself to be initialized upon construction";
+ASSERT_FALSE(dsi_params_init.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when not initialised";
 }
 
+TEST_F(DepthSensorIntegratorTest, CheckParamsNotInitialised)
+{
+std::vector<geo::Vector3> measurements;
+
+//generate input data
+cv::Mat depth;
+geo::Pose3D sensor_pose;
+
+ASSERT_FALSE(dsi_cam_init.isInitialized()) << "depthSensorIntegrator believes itself to be initialized upon construction";
+ASSERT_FALSE(dsi_cam_init.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when not initialised";
+}
+
+TEST_F(DepthSensorIntegratorTest, CheckInitialised)
+{
+std::vector<geo::Vector3> measurements;
+
+//generate input data
+cv::Mat depth;
+geo::Pose3D sensor_pose;
+
+ASSERT_TRUE(dsi_full_init.isInitialized()) << "depthSensorIntegrator believes itself to be initialized upon construction";
+ASSERT_TRUE(dsi_full_init.imageToNavscan(measurements, depth, sensor_pose)) << "imageToNavscan returned true even when not initialised";
+}
 
 int main(int argc, char **argv)
 {
